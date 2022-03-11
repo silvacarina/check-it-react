91c02f2587e7a0b3870e16b836777f608232a873
Carina <cass.carinasilva@gmail.com>
2022-03-10 21:50:57 -0300

    Atualizado componentes 'TaskList' para receber prop 'tasks'

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,13 +5,38 @@ import TasksList from '../TasksList';
 import './style.css';
 
 function App() {
+  const tasksTodo = [
+    {
+      title: 'Comprar pudim',
+      checked: false,
+      id: 1646952830847,
+    },
+    {
+      title: 'Comprar chocolate',
+      checked: false,
+      id: 1646952839645,
+    },
+  ];
+  const tasksDone = [
+    {
+      title: 'Comprar bolo',
+      checked: true,
+      id: 1646952834798,
+    },
+    {
+      title: 'Comer docinhos',
+      checked: true,
+      id: 1646952842062,
+    },
+  ];
+
   return (
     <div className="app">
       <Logo />
       <Form />
       <div className="app__tasks-container">
-        <TasksList title="To-do" />
-        <TasksList title="Done" />
+        <TasksList title="To-do" tasks={tasksTodo} />
+        <TasksList title="Done" tasks={tasksDone} />
       </div>
     </div>
   );
--- src/components/TasksList/index.jsx
+++ src/components/TasksList/index.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
+import propTypes from 'prop-types';
 
-function TaskList({ title }) {
+function TaskList({ title, tasks }) {
   return (
     <section className="tasks-section">
       <h2 className="tasks-section__title">{title}</h2>
       <ul className="tasks-section__list" id="list-todo">
-
-        <li className="tasks-section__item" data-id="1646494862125">
-          <label className="tasks-section__item__label">
-            <input className="tasks-section__item__checkbox" type="checkbox" />
-            <span className="tasks-section__item__text">aa</span>
-          </label>
-          <button type="button" className="tasks-section__item__remove">X</button>
-        </li>
-        <li className="tasks-section__item" data-id="1646494862125">
-          <label className="tasks-section__item__label">
-            <input className="tasks-section__item__checkbox" type="checkbox" />
-            <span className="tasks-section__item__text">aa</span>
-          </label>
-          <button type="button" className="tasks-section__item__remove">X</button>
-        </li>
+        {
+        tasks.map((currentTask) => (
+          <li className="tasks-section__item" key={currentTask.id}>
+            <label className="tasks-section__item__label">
+              <input checked={currentTask.checked} className="tasks-section__item__checkbox" type="checkbox" />
+              <span className="tasks-section__item__text">{currentTask.title}</span>
+            </label>
+            <button type="button" className="tasks-section__item__remove">X</button>
+          </li>
+        ))
+      }
       </ul>
     </section>
   );
@@ -29,6 +26,11 @@ function TaskList({ title }) {
 
 TaskList.propTypes = {
   title: PropTypes.string.isRequired,
+  tasks: PropTypes.arrayOf(propTypes.shape({
+    title: PropTypes.string.isRequired,
+    checked: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired,
+  })).isRequired,
 };
 
 export default TaskList;
